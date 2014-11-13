b830f9c146299d5d0ba64bc3fc2e802a33547bc9
gatimus <william.g.lohan@gmail.com>
2014-11-12 21:23:13 -0800

    11/12 2nd

--- src/com/wl/guessinggame/Game.java
+++ src/com/wl/guessinggame/Game.java
@@ -1,17 +1,39 @@
 package com.wl.guessinggame;
 
-import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Game extends ActionBarActivity {
+public class Game extends ActionBarActivity implements OnClickListener {
+	
+	private final int SETTING_CODE = 2;
+	private int min;
+	private int max;
+	private int number;
+	private int guess;
+	private int score;
+	private Button btnGuess;
+	private EditText inputGuess;
+	private Toast toast;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		min = 0;
+		max = 100;
+		score = 0;
+		btnGuess = (Button) findViewById(R.id.btnGuess);
+		btnGuess.setOnClickListener(this);
+		inputGuess = (EditText) findViewById(R.id.editGuess);
+		newGame();
 	}
 
 	@Override
@@ -37,10 +59,42 @@ public class Game extends ActionBarActivity {
 	}
 	
 	public void openSettings() {
-		Dialog d = new Dialog(getApplicationContext());
-        d.setContentView(R.layout.dialog);
-        d.setTitle("This is custom dialog box");
-        d.show(); 
+		 Intent i = new Intent(getApplicationContext(), Settings.class);
+		 i.putExtra("min", min);
+		 i.putExtra("max", max);
+		 startActivityForResult(i, SETTING_CODE);
+	}
+
+	@Override
+	public void onClick(View v) {
+		score ++;
+		guess = Integer.parseInt(inputGuess.getText().toString());
+		if(guess == number) {
+			win();
+		}
+		else {
+			toast = Toast.makeText(getApplicationContext(), "Score: " + String.valueOf(score), Toast.LENGTH_SHORT);
+			toast.show();
+		}
+	}
+	
+	public void newGame() {
+		number = min + (int)(Math.random() * ((max - min) + 1));
+		score = 0;
+		toast = Toast.makeText(getApplicationContext(), "New Game" + String.valueOf(number), Toast.LENGTH_LONG);
+		toast.show();
+	}
+	
+	public void win() {
+		toast = Toast.makeText(getApplicationContext(), "You Won! In " + String.valueOf(score) + " tries.", Toast.LENGTH_LONG);
+		toast.show();
+		newGame();
+	}
+	
+	@Override
+	public void onActivityResult(int RequestCode, int resultCode, Intent settingData) {
+		super.onActivityResult(RequestCode, resultCode, settingData);
+		
 	}
 	
 }
--- src/com/wl/guessinggame/Settings.java
+++ src/com/wl/guessinggame/Settings.java
@@ -1,38 +1,15 @@
 package com.wl.guessinggame;
 
-import java.util.List;
-
-import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 
-
-public class Settings extends PreferenceActivity {
-	
-	
-	
-	@Override
-    public void onCreate(Bundle savedInstanceState) 
-    {
-            super.onCreate(savedInstanceState);
-    }
-	
+public class Settings extends Activity {
 
 	@Override
-    public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.pref_headers, target);
-    }
-	
-
-	public static class PrefsFragment extends PreferenceFragment {
-		@Override
-	    public void onCreate(Bundle savedInstanceState) 
-	    {
-	            super.onCreate(savedInstanceState);
-	            addPreferencesFromResource(R.xml.pref_general);
-	    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
 	}
-
 }
